65e0912082c95ea6b02a14e34cfb8cd34bd72a1c
Tushar Biswas <tusarbiswas888@gmail.com>
2022-12-18 19:53:56 +0600

    logout, add friend, remove friend developed in backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,4 +33,138 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     }
 
     sendToken(user, 200, res);
+});
+
+// get own user details
+exports.getOwnUserDetails = catchAsyncErrors((req, res, next) => {
+    const user = req.user.toObject();
+
+    delete user.friends;
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+// get user details by others
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id;
+
+    const user = await User.findById(id)
+    if (!user) {
+        return next(new ErrorHandler("User not found.", 404));
+    }
+
+    const modifiedUser = {
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+        location: user.location,
+        occupation: user.occupation,
+        impressions: user.impressions
+    }
+
+    res.status(200).json({
+        success: true,
+        user: modifiedUser
+    })
+});
+
+//Logout a user
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+});
+
+// Add friend 
+exports.addFriend = catchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id;
+    const user = req.user;
+
+    if (user.id === id) {
+        return next(new ErrorHandler("You can't add yourself as a friend.", 400))
+    }
+
+    const friend = await User.findById(id);
+    if (!friend) {
+        return next(new ErrorHandler("User not found in database."));
+    }
+
+    if (user.friends.includes(id)) {
+        return next(new ErrorHandler("User is already in your friendlist", 400));
+    }
+
+    user.friends.push(id);
+    friend.friends.push(user.id);
+
+    await user.save();
+    await friend.save();
+
+    res.status(200).json({
+        success: true,
+        message: "User added to your friend list."
+    })
+})
+
+// Remove a friend
+exports.removeFriend = catchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id;
+    const user = req.user;
+
+    if (user.id === id) {
+        return next(new ErrorHandler("You can't  add or remove yourself as a friend.", 400))
+    }
+
+    const friend = await User.findById(id);
+    if (!friend) {
+        return next(new ErrorHandler("User not found in database."));
+    }
+
+    if (!user.friends.includes(id)) {
+        return next(new ErrorHandler("User isn't in your friendlist", 400));
+    }
+
+    user.friends = user.friends.filter((friend) => friend.toString() !== id);
+    friend.friends = friend.friends.filter((friend) => friend.toString() !== user.id);
+
+    await user.save();
+    await friend.save();
+
+    res.status(400).json({
+        success: true,
+        message: "User removed from your friend list."
+    })
+
+});
+
+// get your friendList
+exports.getFriendList = catchAsyncErrors(async (req, res, next) => {
+    const user = req.user;
+
+    await user.populate("friends")
+
+    const Modifiedfriends = user.friends.map((friend) => {
+        return {
+            id: friend.id,
+            name: friend.name,
+            avatar: friend.avatar,
+            location: friend.location,
+            occupation: friend.occupation,
+            impressions: friend.impressions
+        }
+    })
+
+    res.status(400).json({
+        success: true,
+        friends: Modifiedfriends
+    })
 })
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getOwnUserDetails, getUserDetails, logoutUser, addFriend, removeFriend, getFriendList } = require('../controllers/userController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').get(isAuthenticatedUser, logoutUser);
+router.route('/me').get(isAuthenticatedUser, getOwnUserDetails);
+router.route('/me/friend/add/:id').get(isAuthenticatedUser, addFriend);
+router.route('/me/friend/remove/:id').get(isAuthenticatedUser, removeFriend);
+router.route('/me/friends').get(isAuthenticatedUser, getFriendList);
+router.route('/user/:id').get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
